79abef0dd0408ab7ab9378d6628cf49693e89a2e
Justin Santos <83016833+Ozzie-code-alt@users.noreply.github.com>
2023-07-03 23:30:13 +0800

    Updates

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,26 +1,38 @@
 import { Fragment } from "react"
 
 function ListGroup() {
-const items = [
+let items = [
     'NewYork',
     'SanFrancisco',
     'Philippines',
     'Tokyo',
     'London',
 ]
+items = []
+// const message = items.length ==0 ? <p>No Item </p> : null // make it here
 
+const getMessage = () =>{
+  return items.length ==0 && <p>No Item found </p> ;;    // if condition is true p tag will be rendered other if false noting will be rendered good use of ternary operator
+}
 // items.map(item => <li>{item}</li>)
+{/* Conditional Rendering*/}
+
+// if(items.length ==0)
+// return <><h1>List</h1><p>no Item Found</p></> ; {/* Inside a Fragment*/}
+
 
 
     return (
-        <Fragment> {/* use fragmentation instead* or use <>/}
-            <h1>List</h1> {/* this wont work because because a return will only return 1 element and this gets parsed to js as React.CreateElement('h1')*/}
+        <Fragment>{/* use fragmentation instead* or use <>/}
+             {/* this wont work because because a return will only return 1 element and this gets parsed to js as React.CreateElement('h1')*/}
                 <ul className="list-group">
+                <h1>List</h1>
+                {getMessage()} {/* {message}  render here */}
               {/* <li className="list-group-item">An item</li>
               <li className="list-group-item">A second item</li>
               <li className="list-group-item">A third item</li>
               <li className="list-group-item">A fourth item</li>
-              <li className="list-group-item">And a fifth one</li> */}
+              <li className="list-group-item">And a fifth one</li>  with Braces {} we can render anything dynamicallly instead of just html elements */  }
               {items.map(item => <li key={item}>{item}</li>)}
             </ul>
         </Fragment>
