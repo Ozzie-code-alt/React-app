dbd39dd2e6c29a5e4f919927c6ade9fb7a2e953c
Justin Santos <83016833+Ozzie-code-alt@users.noreply.github.com>
2023-07-14 23:42:07 +0800

    Managing State

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,5 +1,5 @@
 import { Fragment } from "react"
-
+import { MouseEvent } from "react";
 function ListGroup() {
 let items = [
     'NewYork',
@@ -8,7 +8,7 @@ let items = [
     'Tokyo',
     'London',
 ]
-items = []
+// items = []
 // const message = items.length ==0 ? <p>No Item </p> : null // make it here
 
 const getMessage = () =>{
@@ -21,21 +21,35 @@ const getMessage = () =>{
 // return <><h1>List</h1><p>no Item Found</p></> ; {/* Inside a Fragment*/}
 
 
+//seperate function for onClick instead of inline
+// event handler
+const handleClick  = (event: MouseEvent) => console.log(event)  // type anotation in typescript
+
 
     return (
-        <Fragment>{/* use fragmentation instead* or use <>/}
+      <Fragment>
+        {/* use fragmentation instead* or use <>/}
              {/* this wont work because because a return will only return 1 element and this gets parsed to js as React.CreateElement('h1')*/}
-                <ul className="list-group">
-                <h1>List</h1>
-                {getMessage()} {/* {message}  render here */}
-              {/* <li className="list-group-item">An item</li>
+        <ul className="list-group">
+          <h1>List</h1>
+          {getMessage()} {/* {message}  render here */}
+          {/* <li className="list-group-item">An item</li>
               <li className="list-group-item">A second item</li>
               <li className="list-group-item">A third item</li>
               <li className="list-group-item">A fourth item</li>
-              <li className="list-group-item">And a fifth one</li>  with Braces {} we can render anything dynamicallly instead of just html elements */  }
-              {items.map(item => <li key={item}>{item}</li>)}
-            </ul>
-        </Fragment>
-    )
+              <li className="list-group-item">And a fifth one</li>  with Braces {} we can render anything dynamicallly instead of just html elements */}
+          {items.map((item, index) => (
+            <li
+              className="list-group-item"
+              key={item}
+              // onClick={(event) => console.log(item ,index)}
+              onClick={handleClick} //
+            >
+              {item}{" "}
+            </li>
+          ))}
+        </ul>
+      </Fragment>
+    );
 }
 export default ListGroup
